8deb4f3f2e1ac4594de9457659258d9fab3e12b0
martin mauchauffée <moechofe@gmail.com>
2026-02-16 11:48:01 +0100

    remove old Help storyboard and everything that goes with it

--- project.ios/LowResRMX/Scenes/Main/TabBarController.swift
+++ project.ios/LowResRMX/Scenes/Main/TabBarController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 enum TabIndex: Int {
     case explorer
-    case help
+    // case help
     //case about
     //case community
 }
@@ -19,33 +19,33 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         AppController.shared.tabBarController = self
-        
+
         let explorerVC = self.storyboard!.instantiateViewController(withIdentifier: "ExplorerNav")
 
-        let helpStoryboard = UIStoryboard(name: "Help", bundle: nil)
-        let helpVC = helpStoryboard.instantiateInitialViewController()!
-        
+        // let helpStoryboard = UIStoryboard(name: "Help", bundle: nil)
+        // let helpVC = helpStoryboard.instantiateInitialViewController()!
+
         //let aboutVC = self.storyboard!.instantiateViewController(withIdentifier: "AboutNav")
-        
+
         //let communityVC = self.storyboard!.instantiateViewController(withIdentifier: "CommunityNav")
-        
+
         explorerVC.tabBarItem = item(title: "My Programs", imageName: "programs")
-        helpVC.tabBarItem = item(title: "Help", imageName: "help")
+        // helpVC.tabBarItem = item(title: "Help", imageName: "help")
         //aboutVC.tabBarItem = item(title: "About", imageName: "about")
         //communityVC.tabBarItem = item(title: "Community", imageName: "community")
-        
-        self.viewControllers = [explorerVC, helpVC] //, aboutVC, communityVC]
-                
+
+        self.viewControllers = [explorerVC] //, helpVC] //, aboutVC, communityVC]
+
         NotificationCenter.default.addObserver(self, selector: #selector(didAddProgram), name: NSNotification.Name(rawValue: "ProjectManagerDidAddProgram"), object: nil)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         AppController.shared.checkShowProgram()
     }
-    
+
     //override var keyCommands: [UIKeyCommand]? {
     //    if presentedViewController != nil {
     //        return nil
@@ -57,11 +57,11 @@ class TabBarController: UITabBarController {
     //        UIKeyCommand(input: "4", modifierFlags: .command, action: #selector(onTab4), discoverabilityTitle: "Show Community")
     //    ]
     //}
-    
+
     func item(title: String, imageName: String) -> UITabBarItem {
         return UITabBarItem(title: title, image: UIImage(named: imageName), selectedImage: nil)
     }
-    
+
     func dismissPresentedViewController(completion: @escaping () -> Void) {
         var topVC = self.selectedViewController
         if topVC is UINavigationController {
@@ -73,7 +73,7 @@ class TabBarController: UITabBarController {
             completion()
         }
     }
-    
+
     func showExplorer(animated: Bool, root: Bool) {
         self.selectedIndex = TabIndex.explorer.rawValue;
         let nav = self.selectedViewController as? UINavigationController
@@ -87,33 +87,33 @@ class TabBarController: UITabBarController {
             }*/
         }
     }
-    
+
     func showHelp(chapter: String) {
-        self.selectedIndex = TabIndex.help.rawValue;
-        let helpVC = self.selectedViewController as! HelpSplitViewController
-        helpVC.showChapter(chapter)
+        // self.selectedIndex = TabIndex.help.rawValue;
+        // let helpVC = self.selectedViewController as! HelpSplitViewController
+        // helpVC.showChapter(chapter)
     }
-    
+
     @objc func didAddProgram() {
         if self.selectedIndex != 0 {
             self.selectedIndex = 0;
         }
     }
-    
+
     @objc func onTab1() {
         selectedIndex = 0
     }
-    
+
     @objc func onTab2() {
         selectedIndex = 1
     }
-    
+
     //@objc func onTab3() {
     //    selectedIndex = 2
     //}
-    
+
     //@objc func onTab4() {
     //    selectedIndex = 3
     //}
-    
+
 }
