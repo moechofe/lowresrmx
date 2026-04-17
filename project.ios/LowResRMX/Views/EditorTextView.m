5bc8889a851a426693c310458b14c1e8c287966c
martin mauchauffée <moechofe@gmail.com>
2026-04-17 18:25:20 +0200

    colorize on restore

--- project.ios/LowResRMX/Views/EditorTextView.m
+++ project.ios/LowResRMX/Views/EditorTextView.m
@@ -428,8 +428,8 @@
 		}
 	}
 
-	// Colorize ON var GOTO/GOSUB/RESTORE label1, label2, ...
-	NSRegularExpression *onGotoGosubRegex = [NSRegularExpression regularExpressionWithPattern:@"\\bON\\s+[A-Za-z_][A-Za-z0-9_\\(\\)]*\\s+(GOTO|GOSUB|RESTORE)\\s+([A-Za-z_][A-Za-z0-9_]*(?:\\s*,\\s*[A-Za-z_][A-Za-z0-9_]*)*)" options:NSRegularExpressionCaseInsensitive error:nil];
+	// Colorize ON var GOTO/GOSUB label1, label2, ...
+	NSRegularExpression *onGotoGosubRegex = [NSRegularExpression regularExpressionWithPattern:@"\\bON\\s+[A-Za-z_][A-Za-z0-9_\\(\\)]*\\s+(GOTO|GOSUB)\\s+([A-Za-z_][A-Za-z0-9_]*(?:\\s*,\\s*[A-Za-z_][A-Za-z0-9_]*)*)" options:NSRegularExpressionCaseInsensitive error:nil];
 	NSArray<NSTextCheckingResult *> *onGotoGosubMatches = [onGotoGosubRegex matchesInString:text options:0 range:range];
 	for (NSTextCheckingResult *match in onGotoGosubMatches) {
 		BOOL inString = NO;
