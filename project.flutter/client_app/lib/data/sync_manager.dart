584148f334fbda03280a13b1b771b1c08ba4d2f4
martin mauchauffée <moechofe@gmail.com>
2026-04-22 09:59:59 +0200

    try to sync with google drive

--- project.flutter/client_app/lib/data/sync_manager.dart
+++ project.flutter/client_app/lib/data/sync_manager.dart
@@ -1,16 +1,14 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:googleapis/drive/v3.dart';
 import 'package:lowresrmx/data/preference.dart';
 
 class SyncManager with ChangeNotifier {
 	late Future<void> _signInInitialized;
   GoogleSignInAccount? _currentUser;
-  bool _isAuthorized = false;
-  GoogleSignInAccount? get currentUser => _currentUser;
-  bool get isLoggedIn => _currentUser != null;
-  bool get isAuthorized => _isAuthorized;
+	GoogleSignInClientAuthorization? _authorization;
 
   static const List<String> googleScopes = [DriveApi.driveFileScope];
 
@@ -29,25 +27,26 @@ class SyncManager with ChangeNotifier {
     signIn.authenticationEvents.listen((event) async {
       if (event is GoogleSignInAuthenticationEventSignIn) {
         _currentUser = event.user;
-        _isAuthorized = (await _currentUser?.authorizationClient.authorizationForScopes(googleScopes)) != null;
-				MyPreference.setGoogleSigned(_isAuthorized);
+				_requestAuthorization();
       } else if (event is GoogleSignInAuthenticationEventSignOut) {
         _currentUser = null;
-        _isAuthorized = false;
-				MyPreference.setGoogleSigned(_isAuthorized);
+				_authorization = null;
+				MyPreference.setGoogleSigned(false);
+				notifyListeners();
       }
-      notifyListeners();
     });
 
 		_signInInitialized.then((value) async {
 			if (await MyPreference.getGoogleSigned()) {
 				signIn.attemptLightweightAuthentication();
+				_checkAuthorization();
 			}
 		});
   }
 
   Future<void> login() async {
     try {
+      // authenticate() replaces the old signIn() method.
       await GoogleSignIn.instance.authenticate();
     } catch (err) {
       debugPrint("Login error: $err");
@@ -58,10 +57,35 @@ class SyncManager with ChangeNotifier {
     await GoogleSignIn.instance.disconnect();
   }
 
-  Future<void> requestDrivePermissions() async {
-    if (_currentUser == null) return;
-    _isAuthorized = await _currentUser?.authorizationClient.authorizeScopes(googleScopes) != null;
-		MyPreference.setGoogleSigned(_isAuthorized);
-    notifyListeners();
-  }
+	void _updateAuthorization(GoogleSignInClientAuthorization? authorization) {
+		_authorization = authorization;
+
+		if (authorization != null) {
+			MyPreference.setGoogleSigned(true);
+			notifyListeners();
+		}
+	}
+
+  Future<void> _checkAuthorization() async {
+		_updateAuthorization(
+			await _currentUser?.authorizationClient.authorizationForScopes(googleScopes));
+	}
+
+	Future<void> _requestAuthorization() async {
+		_updateAuthorization(await _currentUser?.authorizationClient.authorizeScopes(googleScopes));
+	}
+
+  // /// Returns a [DriveApi] instance if the user is logged in and authorized.
+  // Future<DriveApi?> getDriveApi() async {
+  //   final account = _currentUser;
+  //   if (account == null) return null;
+
+  //   // Ensure we have the necessary scopes
+  //   final isAuthorized = (await account.authorizationClient.authorizationForScopes(googleScopes)) != null;
+  //   if (!isAuthorized) return null;
+
+  //   final client = await account.authClient();
+  //   if (client == null) return null;
+  //   return DriveApi(client);
+  // }
 }
