7a00df94580f3af4d80a60ea0942c291a8e3fc1e
moechofe <art@oui.la>
2026-05-06 20:56:35 +0200

    upd location

--- tool.dev/merge_for_android_export.bash
+++ tool.dev/merge_for_android_export.bash
@@ -5,5 +5,5 @@ ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")"
 INCLUDE="$ROOT/backend.core $ROOT/frontend.sdl"
 DST="$ROOT/export.android/"
 
-bash "$DIR/merge_c_code.bash" "$DIR/android.h" "$ROOT/backend.core $ROOT/frontend.sdl" "$ROOT/export.android/src/all"
+bash "$DIR/merge_c_code.bash" "$DIR/android.h" "$ROOT/backend.core $ROOT/frontend.sdl" "$ROOT/export.android/android-project/app/c/all"
 
