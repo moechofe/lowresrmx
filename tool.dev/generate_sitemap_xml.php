d99707cd7a302310789c43e3103208378f7f41a9
martin mauchauffée <moechofe@gmail.com>
2026-05-13 11:06:16 +0200

    fix sitemap

--- tool.dev/generate_sitemap_xml.php
+++ tool.dev/generate_sitemap_xml.php
@@ -2,23 +2,32 @@
 
 require_once __DIR__.'/redis.php';
 
+defined('WEBSITE_URL') or define('WEBSITE_URL','http://127.0.0.1:8080');
 defined('REDIS_DSN') or define('REDIS_DSN','tcp://127.0.0.1:6379');
 $client=new Client(REDIS_DSN);
 
-$date=date('Y-m-d');
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
 foreach([
-	["community.html","weekly","1.0"],
-	["show.html","weekly","0.8"],
-	["chat.html","weekly","0.8"],
-	["help.html","weekly","0.8"],
-	["documentation.html","monthly","0.4"],
-	["privacy-policy.html","yearly","0.1"],
-	["terms-of-service.html","yearly","0.1"],
+	["community.html","daily","1.0"],
+	["show.html","daily","0.8"],
+	["chat.html","daily","0.8"],
+	["help.html","daily","0.8"],
+	["documentation.html","weekly","0.4"],
+	["privacy-policy.html","monthly","0.1"],
+	["terms-of-service.html","monthly","0.1"],
 ]as[$path,$freq,$prio]):
+
+$hash=sha1(file_get_contents(WEBSITE_URL."/$path"));
+$date=$client->get("m:$hash");
+if(!$date)
+{
+	$date=date('Y-m-d');
+	$client->set("m:$hash",$date);
+}
+
 ?>
 	<url>
 		<loc>https://ret.ro.it/<?=$path?></loc>
