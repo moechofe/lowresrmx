e823e2eb100ff8f3de4851b7f96718c1e17fd803
martin mauchauffée <moechofe@gmail.com>
2026-07-28 16:16:34 +0200

    remove clang-format

--- tool.dev/format_code.bash
+++ tool.dev/format_code.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # apt install clang
-# brew install clang-format
 # brew install fd
 # brew install swiftformat
 # brew install uncrustify
@@ -9,25 +8,6 @@
 ROOT="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")"
 
 cd "$ROOT"
-
-# fd '\.[h|c]$' frontend.sdl backend.core | xargs clang-format --style="{\
-# BasedOnStyle: Microsoft, \
-# IndentWidth: 4, \
-# TabWidth: 4, \
-# UseTab: Always, \
-# SpaceBeforeParens: Never, \
-# BinPackArguments: false, \
-# ContinuationIndentWidth: 0, \
-# AlignAfterOpenBracket: DontAlign, \
-# AlignOperands: DontAlign, \
-# AlignEscapedNewlines: DontAlign, \
-# AlignTrailingComments: {Kind: Never, OverEmptyLines: 0}, \
-# AlignConsecutiveAssignments: None, \
-# AlignConsecutiveDeclarations: None, \
-# AlignConsecutiveMacros: None, \
-# AlignConsecutiveBitFields: None, \
-# AlignArrayOfStructures: None \
-# }" -i
 
 fd '\.swift$' project.ios/LowResRMX project.ios/Widgets | grep -v zstd | xargs -n1 swiftformat --quiet --swift-version 5.9 --allman true  --indent tab
 
