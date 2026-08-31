f70e2e3be75ff8e49372b11f4d0c77ea6046dfa0
martin mauchauffée <moechofe@gmail.com>
2026-08-31 15:24:15 +0200

    up

--- tool.dev/android.h
+++ tool.dev/android.h
@@ -5,6 +5,7 @@
 #include "boot_intro.h"
 #include "core_delegate.h"
 #include "core_stats.h"
+#include "core_syntax.h"
 #include "string_utils.h"
 #include "startup_sequence.h"
 #include "charsets.h"
